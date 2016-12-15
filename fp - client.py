371cc503f8b8e45db663e10806f0ed7169f8e4df
Kevin <kevinfachreza@yahoo.com>
2016-12-15 21:40:40 +0700

    add prevent player pos Y

--- fp - client.py
+++ fp - client.py
@@ -4,7 +4,7 @@ location = [0, 0]
 size = width, height = 1200, 650
 speed = [1, 1]
 black = 0, 0, 0
-move_ticker = 0
+move_tick2log2er = 0
 
 screen = pygame.display.set_mode(size)
 
@@ -16,6 +16,9 @@ player = pygame.image.load("assets/player.png")
 player = pygame.transform.scale(player, (28, 150))
 player_rect = player.get_rect()
 
+#posisi player Y diambil posisi di paling atas dari pemain. Min 0, max 50
+player_posY = str(player_rect.move(location)).split(',')[1]
+
 enemy = pygame.image.load("assets/enemy.png")
 enemy_rect = enemy.get_rect()
 
@@ -25,9 +28,28 @@ while 1:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_DOWN:
-                location[1] = 1
+
+                player_posY = str(player_rect.move(location)).split(',')[1]
+                player_posY = int(player_posY)
+
+                print player_posY
+
+                if player_posY <= 500 :
+                    location[1] = 1
+                else:
+                    location[1] = 0
+
             if event.key == pygame.K_UP:
-                location[1] = -1
+
+                player_posY = str(player_rect.move(location)).split(',')[1]
+                player_posY = int(player_posY)
+
+                print player_posY
+
+                if player_posY >= 0 :
+                    location[1] = -1
+                else:
+                    location[1] = 0
         if event.type == pygame.KEYUP:
             print "berenti oi "+ str(location[1])
             location[1] = 0
