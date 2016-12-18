d0f4ba10709da11bea2630bbda9c259f66d660ea
Kevin <kevinfachreza009@gmail.com>
2016-12-18 14:19:13 +0700

    new assets + player movement

--- fp - client.py
+++ fp - client.py
@@ -2,6 +2,7 @@ import sys, pygame
 pygame.init()
 location = [0, 0]
 size = width, height = 1200, 650
+player_offset = 20
 speed = [1, 1]
 black = 0, 0, 0
 move_tick2log2er = 0
@@ -12,59 +13,95 @@ ball = pygame.image.load("assets/ball.png")
 ballrect = ball.get_rect()
 ballrect.move_ip(400, 350)
 
+#CreateGoal1
+goal1 = pygame.image.load("assets/goal.png")
+goal1 = pygame.transform.scale(goal1, (15, 650))
+goal1_rect = goal1.get_rect()
+goal1_rect = goal1_rect.move(0, 0) #Set PositionGoal1
+
+#CreateGoal1
+goal2 = pygame.image.load("assets/goal.png")
+goal2 = pygame.transform.scale(goal2, (15, 650))
+goal2_rect = goal2.get_rect()
+goal2_rect = goal2_rect.move(1180, 0) #Set PositionGoal2
+
+
+#CreateMidLine
+midline = pygame.image.load("assets/midline.png")
+midline = pygame.transform.scale(goal2, (15, 650))
+midline_rect = midline.get_rect()
+midline_rect = midline_rect.move(590, 0) #Set Midline Pos
+
+#Create Player
 player = pygame.image.load("assets/player.png")
 player = pygame.transform.scale(player, (28, 150))
 player_rect = player.get_rect()
+player_rect = player_rect.move(player_offset,0)
 
 #posisi player Y diambil posisi di paling atas dari pemain. Min 0, max 50
 player_posY = str(player_rect.move(location)).split(',')[1]
 
-enemy = pygame.image.load("assets/enemy.png")
+
+#Create Enemy
+enemy = pygame.image.load("assets/player.png")
+enemy = pygame.transform.scale(enemy, (28, 150))
 enemy_rect = enemy.get_rect()
+enemy_rect = enemy_rect.move(width-(3*player_offset),0)
+
 
 while 1:
+    player_posY = str(player_rect.move(location)).split(',')[1]
+    player_posY = int(player_posY)
+
+    #EVENT KEY DWN AND UP
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_DOWN:
-
-                player_posY = str(player_rect.move(location)).split(',')[1]
-                player_posY = int(player_posY)
-
                 print player_posY
-
-                if player_posY <= 500 :
-                    location[1] = 1
-                else:
-                    location[1] = 0
-
             if event.key == pygame.K_UP:
-
-                player_posY = str(player_rect.move(location)).split(',')[1]
-                player_posY = int(player_posY)
-
                 print player_posY
-
-                if player_posY >= 0 :
-                    location[1] = -1
-                else:
-                    location[1] = 0
         if event.type == pygame.KEYUP:
-            print "berenti oi "+ str(location[1])
+            #print "berenti oi "+ str(location[1])
             location[1] = 0
 
+
+    #EVENT KEY PRESSED BUFFERED
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_DOWN]:
+        print player_posY
+        if player_posY <= 500 :
+            location[1] = 1
+        else:
+            location[1] = 0
+    if keys[pygame.K_UP]:
+        print player_posY
+        if player_posY >= 0:
+            location[1] = -1
+        else:
+            location[1] = 0
+
+    #ANIMATION
     player_rect = player_rect.move(location)
     ballrect = ballrect.move(speed)
 
-    if ballrect.colliderect(player_rect) or ballrect.right > width:
+    #COLLISION
+    if ballrect.colliderect(player_rect) or  ballrect.colliderect(enemy_rect) or ballrect.right > width:
         speed[0] = -speed[0]
     if ballrect.top < 0 or ballrect.bottom > height:
         speed[1] = -speed[1]
 
+    #DRAW THE ASSETS
     screen.fill(black)
+
+    screen.blit(goal1, goal1_rect)
+    screen.blit(goal2, goal2_rect)
+    screen.blit(midline, midline_rect)
+
     screen.blit(ball, ballrect)
     screen.blit(player, player_rect)
+    screen.blit(enemy, enemy_rect)
     pygame.display.flip()
 
 #yang dikirim ke server posisi player, status game
