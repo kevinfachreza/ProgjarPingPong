ad58bf750ff8a7d0adc2ed25a76decdc88bc3a5b
Al Fatih <alfatih_af@yahoo.co.id>
2016-12-20 14:58:14 +0700

    fix client

--- client.py
+++ client.py
@@ -1,4 +1,18 @@
-import sys, pygame, time, msvcrt, socket
+import sys, pygame, time, msvcrt, socket, pickle
+
+
+
+message_in = {"player1_pos" : [0,325],
+                "player2_pos" : [1140,325],
+                "ball_pos" : [600, 325],
+                "score1_val" : 0,
+                "score2_val" : 0,
+                "time" : 0,
+                "win" : 0,
+                "status" : 0}
+
+message_out = { "input_key" : [0,0],
+                "status" : 0}
 
 pygame.init()
 location = [0, 0]
@@ -21,8 +35,6 @@ screen = pygame.display.set_mode(size)
 
 ball = pygame.image.load("assets/bola.png")
 ball = pygame.transform.scale(ball, (30, 30))
-ballrect = ball.get_rect()
-ballrect.move_ip(width/2, height/2)
 
 #CreateGoal1
 goal1 = pygame.image.load("assets/goal.png")
@@ -45,17 +57,11 @@ midline_rect = midline_rect.move(590, 0) #Set Midline Pos
 #Create Player
 player = pygame.image.load("assets/player.png")
 player = pygame.transform.scale(player, (28, 150))
-player_rect = player.get_rect()
-player_rect = player_rect.move(player_offset,0)
 
-#posisi player Y diambil posisi di paling atas dari pemain. Min 0, max 50
-player_posY = str(player_rect.move(location)).split(',')[1]
 
 #Create Enemy
 enemy = pygame.image.load("assets/player.png")
 enemy = pygame.transform.scale(enemy, (28, 150))
-enemy_rect = enemy.get_rect()
-enemy_rect = enemy_rect.move(width-(3*player_offset),0)
 
 #initial score
 score1_val = 0
@@ -70,12 +76,24 @@ seconds = divmod(pygame.time.get_ticks(), 1000)
 timeMatch = myfont.render(str(seconds[0]), 1, BLACK)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+ballrect = ball.get_rect()
+
+
+#Create Player
+player_rect = player.get_rect()
+
+#posisi player Y diambil posisi di paling atas dari pemain. Min 0, max 50
+player_posY = str(player_rect.move(location)).split(',')[1]
+
+#Create Enemy
+enemy_rect = enemy.get_rect()
+
 def loading_page():
     thisfont = pygame.font.Font("assets/HighlandGothicFLF.ttf", 50)
     blink = 1
     enter_msg = "Finding Match"
     global s
-    s.connect(("10.151.43.23", 8008))
+    s.connect(("192.168.43.71", 8008))
 
     while 1:
         s.send('1')
@@ -89,7 +107,8 @@ def loading_page():
         if blink >= 750:
             blink = 0
 
-        if (data == '1'):
+        if (data == '1' or data =='2'):
+            message_out["status"] = data
             break
         else:
             blink = blink + 1
@@ -141,7 +160,13 @@ def countdown_page():
         screen.fill(black)
         screen.blit(msg_enter, (width / 2-50, 125))
         pygame.display.flip()
+    message = {"key" : "ingame"}
+    message.update({"status": message_out["status"]})
+    message.update({"input_key": location})
+    message = pickle.dumps(message)
+    s.send(message)
     game()
+
 
 
 
@@ -192,26 +217,13 @@ def win_page(win):
         screen.blit(msg_enter, (width / 2 - 200, 400))
         pygame.display.flip()
 
-
-def screenblit_game():
-    screen.blit(goal1, goal1_rect)
-    screen.blit(goal2, goal2_rect)
-    screen.blit(midline, midline_rect)
-
-    screen.blit(score1, (width / 2 - 200, 125))
-    screen.blit(score2, (width / 2 + 150, 125))
-
-    pygame.draw.rect(screen, YELLOW, [475, 25, 250, 75])
-    screen.blit(timeMatch, (width / 2 - 25, 25))
-
-    screen.blit(ball, ballrect)
-    screen.blit(player, player_rect)
-    screen.blit(enemy, enemy_rect)
+#
+# def screenblit_game():
 
 
 def game():
     global s
-    global player_posY, player_rect
+    global player_posY, player_rect, enemy_rect
     global ballrect, ball_posX,ball_posY
     global score1_val,score2_val
     global score1, score2, seconds, timeMatch
@@ -219,14 +231,34 @@ def game():
     time = 0;
 
     while 1:
+        datarecv = s.recv(1024)
+        datarecv = pickle.loads(datarecv)
+        print datarecv
 
-        s.send("ingame")
-        time+=1
+        temp = ballrect.move(0,0)
+        ball_posX = datarecv["ball_pos"][0]-temp[0]
+        ball_posY = datarecv["ball_pos"][1]-temp[1]
+
+        temp_player = player_rect.move(0,0)
+        player_posX = datarecv["player1_pos"][0] - temp_player[0]
+        player_posY = datarecv["player1_pos"][1] - temp_player[1]
+
+
+        temp_enemy = enemy_rect.move(0,0)
+        enemy_PosX = datarecv["player2_pos"][0] - temp_enemy[0]
+        enemy_PosY = datarecv["player2_pos"][1] - temp_enemy[1]
+
+        ballrect.move_ip(ball_posX,ball_posY)
+        player_rect = player_rect.move(player_posX, player_posY)
+        enemy_rect = enemy_rect.move(enemy_PosX, enemy_PosY)
+
+
+        message = {"key" : "ingame"}
+        time=datarecv["time"]
         seconds = time/200;
-        print time
+        #print time
         player_posY = str(player_rect.move(location)).split(',')[1]
         player_posY = int(player_posY)
-
         ball_posX = str(ballrect.move(speed)).split(',')[0]
         ball_posY = str(ballrect.move(location)).split(',')[1]
         ball_posX = str(ball_posX).split('(')[1]
@@ -236,28 +268,18 @@ def game():
         #TIME MATCH
         timeMatch = myfont.render(str(seconds), 1, BLACK)
 
-        if score1_val == 10:
-            win = 1
-        elif score2_val == 10:
-            win = 0
 
-        if(score1_val==10 or score2_val==10):
+        if(datarecv["win"]!=0):
+            if(str(datarecv["win"])==str(message_out["status"])):
+                win = 1
+            else:
+                win = 0
             win_page(win)
 
-        #SCORE
-        if(ball_posX < -2):
-            ballrect.move_ip(width/2, (height/2)-int(ball_posY) )
-            player_rect.move_ip(0,0)
-            enemy_rect.move_ip(0,0)
-            score2_val += 1
-        elif ball_posX > width+2:
-            ballrect.move_ip(-width/2, (height/2)-int(ball_posY) )
-            player_rect.move_ip(0,0)
-            enemy_rect.move_ip(0,0)
-            score1_val += 1
 
-        score1 = myfont.render(str(score1_val), 1, WHITE)
-        score2 = myfont.render(str(score2_val), 1, WHITE)
+
+        score1 = myfont.render(str(datarecv["score1_val"]), 1, WHITE)
+        score2 = myfont.render(str(datarecv["score2_val"]), 1, WHITE)
 
         #EVENT KEY DWN AND UP
         for event in pygame.event.get():
@@ -266,23 +288,18 @@ def game():
                 #print "berenti oi "+ str(location[1])
                 location[1] = 0
 
-
         #EVENT KEY PRESSED BUFFERED
         keys = pygame.key.get_pressed()
         if keys[pygame.K_DOWN]:
-            if player_posY <= 500 :
-                location[1] = 1
-            else:
-                location[1] = 0
+            location[1] = 3
         if keys[pygame.K_UP]:
-            if player_posY >= 0:
-                location[1] = -1
-            else:
-                location[1] = 0
+            location[1] = -3
 
+        message.update({"input_key" : location})
+        message.update({"status": message_out["status"]})
         #ANIMATION
         player_rect = player_rect.move(location)
-        ballrect = ballrect.move(speed)
+        ballrect = ballrect.move(datarecv["speed"][0], datarecv["speed"][0])
 
         #COLLISION
         #   colide player
@@ -294,9 +311,24 @@ def game():
         #DRAW THE ASSETS
         screen.fill(black)
 
-        screenblit_game()
+        screen.blit(goal1, goal1_rect)
+        screen.blit(goal2, goal2_rect)
+        screen.blit(midline, midline_rect)
 
+        screen.blit(score1, (width / 2 - 200, 125))
+        screen.blit(score2, (width / 2 + 150, 125))
+
+        pygame.draw.rect(screen, YELLOW, [475, 25, 250, 75])
+        screen.blit(timeMatch, (width / 2 - 25, 25))
+
+        screen.blit(ball, ballrect)
+        screen.blit(player, player_rect)
+        screen.blit(enemy, enemy_rect)
+
+        print(message)
+        dataPickle = pickle.dumps(message)
         pygame.display.flip()
+        s.send(dataPickle)
 #yang dikirim ke server posisi player, status game
 #yg diterima dari server posisi musuh, status game
 
